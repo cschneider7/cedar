91b3c9df3aea2faeea69bbd128381e62e8738760
Casey Schneider <caseyschneider77@gmail.com>
2026-08-22 22:13:37 -0700

    Remove link from logo splash screen
    Signed-off-by: Casey Schneider <caseyschneider77@gmail.com>

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@clerk/react-router"
 import { getAuth } from "@clerk/react-router/server"
-import { Link, redirect } from "react-router"
+import { redirect } from "react-router"
 import { ThemeToggle } from "~/components/ui/theme-toggle"
 import { Wordmark } from "~/components/wordmark"
 import { useClerkAppearance } from "~/hooks/use-clerk-appearance"
@@ -23,9 +23,7 @@ export default function Login() {
       <div className="absolute top-4 right-4">
         <ThemeToggle />
       </div>
-      <Link to="/">
-        <Wordmark textClassName="font-medium" />
-      </Link>
+      <Wordmark textClassName="font-medium" />
       <SignIn
         routing="hash"
         fallbackRedirectUrl="/"
